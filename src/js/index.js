deb73f8d3d881c2748d20e7c9964b1f3cba2bfe4
deadit <d-usov@city-mobil.ru>
2020-04-26 12:13:43 +0300

    update site

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,25 @@
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/tabs';
-import Typed from 'typed.js';
-import { downloader } from './downloadMore';
+import $ from "jquery";
+import "jquery-ui/ui/widgets/tabs";
+import Typed from "typed.js";
+import { downloader } from "./downloadMore";
 
 const dropDownFunc = (id) => {
-  $(`#${id}`).closest('.dropdown').toggleClass('show');
+  $(`#${id}`).closest(".dropdown").toggleClass("show");
 };
 
-
 const copyToClipboard = (str) => {
-  const el = document.createElement('textarea');
+  const el = document.createElement("textarea");
   el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
   document.body.appendChild(el);
-  const selected = document.getSelection().rangeCount > 0
-    ? document.getSelection().getRangeAt(0)
-    : false;
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false;
   el.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(el);
   if (selected) {
     document.getSelection().removeAllRanges();
@@ -27,27 +27,30 @@ const copyToClipboard = (str) => {
   }
 };
 
-window.addEventListener('click', (event) => {
-  if (!event.target.matches('.dropdown__button')) {
-    $('.dropdown').each(function () {
-      $(this).removeClass('show');
+window.addEventListener("click", (event) => {
+  if (!event.target.matches(".dropdown__button")) {
+    $(".dropdown").each(function () {
+      $(this).removeClass("show");
     });
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  if ('fonts' in document) {
-    const regular = new FontFace('LabGrotesque', "url(/fonts/LabGrotesque-Regular-hint-all.woff) format('woff')");
+document.addEventListener("DOMContentLoaded", () => {
+  if ("fonts" in document) {
+    const regular = new FontFace(
+      "LabGrotesque",
+      "url(/fonts/LabGrotesque-Regular-hint-all.woff) format('woff')"
+    );
     regular.load().then((font) => {
       document.fonts.add(font);
     });
   }
-  const typedId = 'typed';
+  const typedId = "typed";
   const typedElement = document.getElementById(typedId);
 
   if (typedElement) {
     new Typed(`#${typedId}`, {
-      stringsElement: '#typed-strings',
+      stringsElement: "#typed-strings",
       typeSpeed: 40,
       backSpeed: 15,
       backDelay: 1200,
@@ -55,44 +58,48 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  $('.dropdown .dropdown__button').each(function (e) {
-    this.addEventListener('click', (e) => {
+  $(".dropdown .dropdown__button").each(function (e) {
+    this.addEventListener("click", (e) => {
       dropDownFunc(e.target.dataset.targetId);
     });
   });
 
-  $('.eg-navbar__trigger').click(() => {
-    $('html').toggleClass('show-main-nav');
+  $(".eg-navbar__trigger").click(() => {
+    $("html").toggleClass("show-main-nav");
   });
 
-  $('.tabs').tabs();
+  $(".tabs").tabs();
 
-  if (document.getElementsByClassName('download').length > 0) {
-    downloader('download', 6);
+  if (document.getElementsByClassName("download").length > 0) {
+    downloader("download", 6);
   }
 
-  if (document.getElementsByClassName('related').length > 0) {
-    downloader('related', 3);
+  if (document.getElementsByClassName("related").length > 0) {
+    downloader("related", 3);
   }
 
-  if (document.getElementsByClassName('copy-link').length > 0) {
-    $('.copy-link').click((e) => {
+  if (document.getElementsByClassName("copy-link").length > 0) {
+    $(".copy-link").click((e) => {
       e.preventDefault();
       copyToClipboard(e.currentTarget.href);
     });
   }
 
-  if (document.getElementsByClassName('expandableCollapsibleDiv').length > 0) {
-    $('.expandableCollapsibleDiv h3').click((e) => {
-      const showElementDescription = $(e.currentTarget).parent('.expandableCollapsibleDiv').find('p');
-      const elementTitle = $(e.currentTarget).parent('.expandableCollapsibleDiv').find('h3');
+  if (document.getElementsByClassName("expandableCollapsibleDiv").length > 0) {
+    $(".expandableCollapsibleDiv h3").click((e) => {
+      const showElementDescription = $(e.currentTarget)
+        .parent(".expandableCollapsibleDiv")
+        .find("p");
+      const elementTitle = $(e.currentTarget)
+        .parent(".expandableCollapsibleDiv")
+        .find("h3");
 
-      if ($(showElementDescription).is(':visible')) {
-        elementTitle.removeClass('active');
+      if ($(showElementDescription).is(":visible")) {
+        elementTitle.removeClass("active");
         showElementDescription.slideUp();
       } else {
         showElementDescription.slideDown();
-        elementTitle.addClass('active');
+        elementTitle.addClass("active");
       }
     });
   }
