e17b200e25481a4c5dfb6966560005e07408da37
deadit <d-usov@city-mobil.ru>
2020-04-29 20:10:31 +0300

    update site

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,81 @@
-const gulp = require('gulp');
-const webpackStream = require('webpack-stream');
-const webpack = require('webpack');
+const gulp = require("gulp");
+const webpackStream = require("webpack-stream");
+const webpack = require("webpack");
 
 function swallowError(error) {
   // If you want details of the error in the console
   console.log(error.toString());
 
-  this.emit('end');
+  this.emit("end");
 }
 
-gulp.task('js', () => gulp.src('./src/js/index.js')
-  .pipe(webpackStream({
-    mode: 'production',
-    output: {
-      filename: 'index.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js)$/,
-          exclude: /(node_modules)/,
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/preset-env'],
-          },
+gulp.task("js", () =>
+  gulp
+    .src("./src/js/index.js")
+    .pipe(
+      webpackStream({
+        mode: "production",
+        output: {
+          filename: "index.js",
         },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.svg$/,
-          use: [
+        module: {
+          rules: [
             {
-              loader: 'file-loader',
+              test: /\.(js)$/,
+              exclude: /(node_modules)/,
+              loader: "babel-loader",
+              query: {
+                presets: ["@babel/preset-env"],
+              },
+            },
+            {
+              test: /\.css$/,
+              use: ["style-loader", "css-loader"],
+            },
+            {
+              test: /\.svg$/,
+              use: [
+                {
+                  loader: "file-loader",
+                },
+              ],
+            },
+            {
+              test: /\.(otf|eot|ttf|woff|woff2)$/,
+              use: {
+                loader: "file-loader",
+              },
+            },
+            {
+              test: /\.(jpg|jpeg|gif|png)$/,
+              use: [
+                {
+                  loader: "file-loader",
+                },
+              ],
             },
           ],
         },
-        {
-          test: /\.(otf|eot|ttf|woff|woff2)$/,
-          use: {
-            loader: 'file-loader',
-          },
-        },
-        {
-          test: /\.(jpg|jpeg|gif|png)$/,
-          use: [
-            {
-              loader: 'file-loader',
-            },
-          ],
-        },
-      ],
-    },
-    externals: ['tls', 'net', 'fs'],
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'jquery-ui': 'jquery-ui',
-        'jquery-collapse': 'jquery-collapse',
-      }),
-    ],
-    // devtool: 'source-map',
-  }))
-  .on('error', swallowError)
-  .pipe(gulp.dest('static/js')));
+        externals: ["tls", "net", "fs"],
+        plugins: [
+          new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "jquery-ui": "jquery-ui",
+            "jquery-collapse": "jquery-collapse",
+            "jquery-lazy": "jquery-lazy",
+          }),
+        ],
+        // devtool: 'source-map',
+      })
+    )
+    .on("error", swallowError)
+    .pipe(gulp.dest("static/js"))
+);
 
-gulp.task('watch', () => {
-  gulp.watch('src/js/**/*.js', gulp.series('js'));
-  gulp.watch('layouts/**/*.html');
+gulp.task("watch", () => {
+  gulp.watch("src/js/**/*.js", gulp.series("js"));
+  gulp.watch("layouts/**/*.html");
 });
 
-gulp.task('build', gulp.series('js'));
+gulp.task("build", gulp.series("js"));
--- src/js/downloadMore.js
+++ src/js/downloadMore.js
@@ -10,12 +10,15 @@ export class Downloader {
       return;
     }
 
-    const nextIndex = (this.currentIndex + quantity) < this.itemsLength
-      ? this.currentIndex + quantity
-      : this.itemsLength;
+    const nextIndex =
+      this.currentIndex + quantity < this.itemsLength
+        ? this.currentIndex + quantity
+        : this.itemsLength;
 
     for (let i = this.currentIndex; i < nextIndex; i += 1) {
-      document.querySelectorAll(`.${this.id}__post`)[i].classList.remove(classes);
+      document
+        .querySelectorAll(`.${this.id}__post`)
+        [i].classList.remove(classes);
     }
 
     return new Downloader(this.id, nextIndex);
@@ -23,22 +26,25 @@ export class Downloader {
 }
 
 export const hideAllPosts = (id, quantity) => {
-  document.querySelectorAll(`.${id} > .${id}__post`)
-    .forEach(
-      (x, key) => (key < quantity ? x.classList.add('visible-hidden') : x.classList.add('d-none')),
+  document
+    .querySelectorAll(`.${id} > .${id}__post`)
+    .forEach((x, key) =>
+      key < quantity
+        ? x.classList.add("visible-hidden")
+        : x.classList.add("d-none")
     );
 };
 
 export const downloader = (id, step) => {
   hideAllPosts(id, step);
-  let currentNews = new Downloader(`${id}`, 0).showNext(step, 'visible-hidden');
+  let currentNews = new Downloader(`${id}`, 0).showNext(step, "visible-hidden");
 
   if (currentNews.currentIndex !== currentNews.itemsLength) {
-    document.querySelector(`.${id}-button`).classList.remove('d-none');
-    document.querySelector(`.${id}-button`).addEventListener('click', (e) => {
-      currentNews = currentNews.showNext(step, 'd-none');
+    document.querySelector(`.${id}-button`).classList.remove("d-none");
+    document.querySelector(`.${id}-button`).addEventListener("click", (e) => {
+      currentNews = currentNews.showNext(step, "d-none");
       if (currentNews.currentIndex === currentNews.itemsLength) {
-        e.target.classList.add('d-none');
+        e.target.classList.add("d-none");
       }
     });
   }
