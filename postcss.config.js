0359b4ed90a89587b8355de006747d6d585cb441
deadit <d-usov@city-mobil.ru>
2020-05-09 16:40:37 +0300

    update site

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,5 @@
 module.exports = {
   plugins: {
     autoprefixer: { browsers: ['last 2 versions', 'iOS >= 8'] },
-    '@fullhuman/postcss-purgecss': {
-      content: ["./layouts/**/*.html", "./components/**/**/*.html"],
-      whitelist: [
-        'highlight',
-        'language-bash',
-        'pre',
-        'code',
-        'content',
-        'h3',
-        'h4',
-        'ul',
-        'li'
-      ]
-    },
   },
 };
