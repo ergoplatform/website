15a89202d200c06f2e70665a9d89e310c41b756b
Ilya Sviridov <sviridov9199@gmail.com>
2021-01-21 20:41:12 +0300

    Revert "add functional for mining calculator"
    This reverts commit 28e75c787e00461aacd9c92f49e0cabeeea4b327.

--- src/js/miningCalculator/src/App.svelte
+++ src/js/miningCalculator/src/App.svelte
@@ -1,113 +1,30 @@
 <script>
-	let message = 'loading...';
-	let currentPrice = null;
-	let currentBlockRewardERG = 67.5;
-	let currentBlockReward = null;
-	let networkHashrate = null;
-	let dailyRevenueERG = null;
-	let dailyRevenue = null;
-
-	const blockTime = 120;
-	const blocksPerDay = 864e2 / blockTime;
-	const yourHashrateInput = document.getElementById("your-hashrate");
-
-	function changedYourHashrate() {
-		const yourHashrate = +yourHashrateInput.value * 1e6;
-		dailyReward = currentBlockRewardERG * yourHashrate * blockTime / difficulty * blocksPerDay;
-		dailyRevenueERG = dailyReward.toFixed(2);
-		dailyRevenue = (dailyReward * currentPrice).toFixed(2);
-	}
-
-	fetch(`https://api.coingecko.com/api/v3/simple/price?ids=ergo&vs_currencies=USD`)
-	.then(response => {
-		if (response.ok) {
-			return response;
-		}
-		else {
-			const error = new Error('Error ' + response.status + ': ' + response.statusText);
-			throw error;
-		}
-	}, error => {
-		return new Error('Error: ' + error.message);
-	})
-	.then(response => response.json())
-	.then(response => {
-		currentPrice = response.ergo.usd.toFixed(2);
-		currentBlockReward = (currentBlockRewardERG * currentPrice).toFixed(2);
-
-		fetch(`https://api.ergoplatform.com/blocks?sortBy=height&sortDirection=desc&limit=1`)
-		.then(response => {
-			if (response.ok) {
-				return response;
-			}
-			else {
-				const error = new Error('Error ' + response.status + ': ' + response.statusText);
-				throw error;
-			}
-		}, error => {
-			return new Error('Error: ' + error.message);
-		})
-		.then(response => response.json())
-		.then(response => {
-			const difficulty = response.items[0].difficulty;
-			networkHashrate = (difficulty / blockTime / 1e12).toFixed(2);
-			changedYourHashrate();
-		})
-		.catch(error => {
-			message = error.message;
-		});
-	})
-	.catch(error => {
-		message = error.message;
-	});
+	export let name;
 </script>
 
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
 <style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
-
-<div>
-	<h1>Ergo Mining Calculator</h1>
-	<div>
-		<div>
-			Current price: 
-			{#if currentPrice} 
-				1 ERG = ${currentPrice}
-			{:else} 
-				{message}
-			{/if}
-		</div>
-
-		<div>
-			Current block reward: 
-			{#if currentBlockRewardERG && currentBlockReward} 
-				{currentBlockRewardERG} ERG = {currentBlockReward}
-			{:else} 
-				{message}
-			{/if}
-		</div>
-
-		<div>
-			Network hashrate: 
-			{#if networkHashrate} 
-				{networkHashrate} TH/s
-			{:else}
-				{message}
-			{/if}
-		</div>
-
-		<div>
-			<span>Your hashrate: </span>
-			<input type="number" value="0" on:change={changedYourHashrate}/>
-			<span>MH/s</span>
-		</div>
-
-		<div>
-			Daily revenue:
-			{#if dailyRevenueERG && dailyRevenue} 
-				{dailyRevenueERG} ERG = ${dailyRevenue}
-			{:else}
-				{message}
-			{/if}
-		</div>
-	</div>
-</div>
--- src/js/miningCalculator/src/main.js
+++ src/js/miningCalculator/src/main.js
@@ -1,7 +1,10 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.querySelector('#miningCalculator')
+	target: document.body,
+	props: {
+		name: 'world'
+	}
 });
 
 export default app;
