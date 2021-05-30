bacb48e2031dc68eb897eac4f7ccb34066ae7e2d
deadit <stdeadit@gmail.com>
2021-05-30 21:20:21 +0300

    update deploy

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Start deploy"
-ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/website; git pull;  make site"'
+ssh -tq root@88.198.50.217 '/bin/bash -l -c "source ~/.nvm/nvm.sh; cd /var/www/html/website; git pull; rm -rf /public;  make site"'
 echo "Deployed Successfully!"
 
 exit 0
