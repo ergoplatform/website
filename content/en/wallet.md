ae467d5194b99b9497999f48b25ac525a0958582
deadit <stdeadit@gmail.com>
2021-10-12 11:00:38 +0300

    add wallet_android page

---
+++ content/en/wallet.md
@@ -0,0 +1,7 @@
+---
+title: "Wallet"
+subtitle: "Wallet page"
+layout: "wallet"
+date: 2019-04-24T13:59:34+03:00
+draft: true
+---
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: {
-    autoprefixer: { browsers: ['last 2 versions', 'iOS >= 8'] },
+    autoprefixer: { },
+    tailwindcss: {},
   },
 };
